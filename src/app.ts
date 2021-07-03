a701f31eea7277a9eda06ca439539fff79c19a7b
Kartik-57 <mt1200812@maths.iitd.ac.in>
2021-07-03 22:11:28 +0530

    Setup EJS and Basic template for LinkTree

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,26 @@
 import * as express from 'express';
+import path = require('path')
 
 // Create Express server.
 const app = express();
 
 // Express configuration
 app.set('port', process.env.PORT || 5000);
+app.set('views', path.join(__dirname, '../../views'));
 
 app.get('/', (req, res) => {
   res.send('Link Logger is Active!');
 });
+
+app.get('/LinkTree', (req, res) => {
+  res.render('LinkTree', {
+    title : 'LinkTree', 
+    username: 'UserName',
+    links: ['link1', 'link2', 'link3', 'link4']
+  });
+});
+
+app.set('view engine', 'ejs');
 
 const server = app.listen(app.get('port'), () => {
   console.log(`App is Running at http://localhost:${app.get('port')}`);
