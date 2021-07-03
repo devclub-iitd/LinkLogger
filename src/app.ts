b46b33dc2432c3d44fd2b3f9a29898ddf960da6b
AwesomePaneer <rajnikunj2002@gmail.com>
2021-07-03 18:22:37 +0530

    Eslint and prettier fixes

--- src/app.ts
+++ src/app.ts
@@ -6,52 +6,58 @@ import linkMap from './models/link';
 const app = express();
 const mongoose = require('mongoose');
 
-var dbURI = "mongodb+srv://test_user:linklogging1234@linklogging.ijmqm.mongodb.net/link_logging?retryWrites=true&w=majority";
-mongoose.connect(dbURI,{ useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
-  .then(function(result:string){
-    console.log("Connected to db");
-  }).catch(function(error:string){
+const dbURI =
+  'mongodb+srv://test_user:linklogging1234@linklogging.ijmqm.mongodb.net/link_logging?retryWrites=true&w=majority';
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then((result: string) => {
+    console.log('Connected to db');
+  })
+  .catch((error: string) => {
     console.log(error);
   });
 
 // Express configuration
 app.set('port', process.env.PORT || 5000);
-app.set('views',path.join(__dirname,"../../views"));
+app.set('views', path.join(__dirname, '../../views'));
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
   res.send('Link Logger is Active!');
 });
 
-app.get('/link_generator',function(req,res){
+app.get('/link_generator', (req, res) => {
   res.render('link_generator');
 });
 
-app.post('/link_generator',function(req,res){
+app.post('/link_generator', (req, res) => {
   const original_link = req.body.original_link;
   const short_link = req.body.short_link;
   const link = new linkMap({
-    short_link:short_link,
-    original_link:original_link
+    short_link: short_link,
+    original_link: original_link,
   });
   link.save();
 });
 
-app.get('/redirect_to/:short_link',function(req,res){
+app.get('/redirect_to/:short_link', (req, res) => {
   const short_link = req.params.short_link;
-  linkMap.findOne({"short_link":short_link})
-    .then((result:typeof linkMap)=>{
-      console.log(result);
-      const original_link = result.original_link;
-      const str = "https://www." + original_link;
-      res.status(301).redirect(str);
-      res.end();
-    })
+  linkMap.findOne({short_link: short_link}).then((result: typeof linkMap) => {
+    console.log(result);
+    const original_link = result.original_link;
+    const str = 'https://www.' + original_link;
+    res.status(301).redirect(str);
+    res.end();
+  });
 });
 
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 const server = app.listen(app.get('port'), () => {
   console.log(`App is Running at http://localhost:${app.get('port')}`);
--- src/models/link.ts
+++ src/models/link.ts
@@ -1,15 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const linkSchema = new Schema({
-  short_link: {
-    type:String,
-    required:true
+const linkSchema = new Schema(
+  {
+    short_link: {
+      type: String,
+      required: true,
+    },
+    original_link: {
+      type: String,
+      required: true,
+    },
   },
-  original_link: {
-    type:String,
-    required:true
-  }
-},{timestamps:true});
+  {timestamps: true}
+);
 
-const linkMap = mongoose.model('Link_map',linkSchema);
+const linkMap = mongoose.model('Link_map', linkSchema);
 export default linkMap;
