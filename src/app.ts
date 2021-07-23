bd6bde314f0e740769ea4ba249cd6ad51a86c0b0
AwesomePaneer <rajnikunj2002@gmail.com>
2021-07-23 13:43:28 +0530

    Added logging

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,17 @@ import * as express from 'express';
 import path = require('path');
 import linkMap from './models/link';
 import User from './models/user';
+import linkData from './models/link_data'
 import auth from './middleware/auth';
+import { Request,Response } from 'express-serve-static-core';
 const cookieParser = require('cookie-parser');
 
 // Create Express server.
 const app = express();
 const mongoose = require('mongoose');
+
+const useragent = require('useragent')
+useragent(true)
 
 const dbURI =
   'mongodb+srv://test_user:linklogging1234@linklogging.ijmqm.mongodb.net/link_logging?retryWrites=true&w=majority';
@@ -45,7 +50,7 @@ app.post('/link_generator', auth, (req, res) => {
   const userData = res.locals.user;
   const original_link = req.body.original_link;
   const short_link = req.body.short_link;
-
+  //have to add user id to users array in link also
   const link = new linkMap({
     short_link: short_link,
     original_link: original_link,
@@ -107,15 +112,41 @@ app.post('/profile/deleteLink', auth, (req, res) => {
   linkMap.findByIdAndDelete(linkObj.id);
 });
 
-app.get('/redirect_to/:short_link', (req, res) => {
+function log_user_data(req:Request,res:Response,result:typeof linkMap){
+  const user_agent_details = useragent.parse(req.headers['user-agent'])
+  const os = user_agent_details.os.toString()
+  const device = user_agent_details.device.toString()
+  const browser = user_agent_details.toAgent()
+  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  const referrer = req.get('Referrer') //equivalent to req.headers.referrer || req.headers.referer
+  const user_data = res.locals.user
+  const link_data = new linkData({
+    link:result._id,
+    operating_system:os,
+    device:device,
+    browser:browser,
+    ip:ip,
+    referrer:referrer,
+    user_data:user_data
+  })
+  link_data.save()
+}
+
+app.get('/redirect_to/:short_link', auth, (req, res) => {
   const short_link = req.params.short_link;
-  linkMap.findOne({short_link: short_link}).then((result: typeof linkMap) => {
-    console.log(result);
+  linkMap.findOne({short_link: short_link})
+  .then((result: typeof linkMap) => {
+    if(!result){
+      throw new Error('Link not found')
+    }
+    log_user_data(req,res,result);
     const original_link = result.original_link;
-    const str = original_link;
-    res.status(301).redirect(str);
+    res.status(301).redirect(original_link);
     res.end();
-  });
+  })
+  .catch(function(err:Error){
+    res.end('Link not found')
+  })
 });
 
 app.set('view engine', 'ejs');
